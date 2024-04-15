01bd70e4a613208534d6e36cbd3d3f7ba6c826dd
mahdi <mmiq69@gmail.com>
2024-04-15 15:56:50 -0400

    initial

---
+++ config/index.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL, {
+        dialect: 'postgres',
+            ssl: true,
+    });
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'postgres',
+            port: 5432
+        }
+    );
+}
+
+module.exports = sequelize;
---
+++ db/schema.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS Postgres_db;
+
+-- Create a new database
+CREATE DATABASE Postgres_db;
